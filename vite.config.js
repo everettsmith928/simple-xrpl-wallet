5376fde208a634232297d529246b888e49692ae8
everettsmith928 <36865028+everettsmith928@users.noreply.github.com>
2024-01-11 16:45:37 -0700

    rebuild

--- vite.config.js
+++ vite.config.js
@@ -24,7 +24,7 @@ const viteConfig = ({ mode }) => {
       },
     },
     build: {
-      outDir: '/docs',
+      outDir: 'docs',
       rollupOptions: {
         plugins: [polyfillNode()],
       },
